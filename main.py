eb60876cd8be141f7d674e7b911dbdaf64da8831
hehehaha-developer <owenpatrickross13+t@gmail.com>
2024-12-28 19:02:45 +1000

    uuuuu

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
-#this is the main file
-#main.py was constructed completely bythe hehehaha developer
+# this is the main file
+# main.py was constructed completely bythe hehehaha developer
 
-#this is not the full game this is a cli knockoff
+# this is not the full game this is a cli knockoff
+
+# variebles
+
+
+sr = " "    # score read from file
+s = 0       # score
+
+stxt = open("score.txt" , "a")    # score data file
+
+
+# function definitions
+
+
+def upscore(n):    # adding the n value to the score
+    sr = int(stxt.readline([1]))
+    sr = sr + n
+    stxt.write(sr) # update the score
+
+def 
+
+# main game code
+
